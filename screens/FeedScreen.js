8e1b2fae1cae6092a9153716c711ab5bbbdeca31
gracjaan <gchmielnicki3005@gmail.com>
2023-02-22 20:24:19 +0100

    Feed displays photos from database

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,19 +1,48 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
 
 const FeedScreen = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    // Initialize Firebase Storage
+    const storageRef = firebase.storage().ref();
+
+    // Get a list of all images in the "images" folder
+    storageRef.child('images').listAll().then(function (result) {
+      result.items.forEach(function (imageRef) {
+        // Get the download URL for each image and add it to the images state
+        imageRef.getDownloadURL().then(function (url) {
+          setImages(images => [...images, { url }]);
+        });
+      });
+    });
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>FeedScreen</Text>
+      <FlatList
+        data={images}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Image style={styles.image} source={{ uri: item.url }} />}
+      />
     </SafeAreaView>
   )
 }
 
+
 export default FeedScreen
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'cover',
+  }
 })
